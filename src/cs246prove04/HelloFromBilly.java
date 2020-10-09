e9328430bf356b7d8eabb656eb3b7dd4ce8292fd
Billy Estella <billyestella02@gmail.com>
2020-10-09 17:56:33 +0800

    Initial commit

---
+++ src/cs246prove04/HelloFromBilly.java
@@ -0,0 +1,10 @@
+package cs246prove04;
+
+public class HelloFromBilly {
+
+    public static void main(String[] args) {
+	// write your code here
+        String name = "Billy";
+        System.out.println("Hello, my name is " + name);
+    }
+}
